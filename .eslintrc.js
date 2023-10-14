50f363c33efb48506b41f8f10e5de0f834c7e51d
Sarah Bunker <ChemEnCoder@gmail.com>
2023-10-13 18:17:56 -0600

    removing semi colon linting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,10 +31,6 @@ module.exports = {
       "error",
       "single"
     ],
-    "semi": [
-      "error",
-      "always"
-    ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": [
